f1190810d86500b852fdd4a8b18a327348ad9253
santhosh.ss <santhosh.ss@zohocorp.com>
2020-01-25 20:07:51 +0530

    db trials

--- djangotest1/myapp/url.py
+++ djangotest1/myapp/url.py
@@ -5,5 +5,7 @@ from myapp import views as myapp_views
 urlpatterns = [
     url(r'^hello/', myapp_views.hello, name='hello'),
     url(r'^contact/', myapp_views.contact, name='contact'),
-    url(r'^article/(\d+)/', myapp_views.article, name='article')
+    url(r'^article/(\d+)/', myapp_views.article, name='article'),
+    url(r'^dbops/', myapp_views.dbops, name='dbops'),
+    url(r'^showallentries/', myapp_views.showallentries, name='showallentries'),
 ]
--- djangotest1/myapp/views.py
+++ djangotest1/myapp/views.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from myapp.models import testModelView
+
 
 def hello(request):
     time = datetime.now().time()
@@ -16,3 +18,49 @@ def contact(request):
 def article(request, articleId):
     text = "Displaying article Number : %s" % articleId
     return HttpResponse(text)
+
+
+def dbops(request):
+    # adding entry here
+
+    testentry = testModelView(
+        website="www.google.com", mail="test@google.com", name="test", phonenumber="1234567890"
+    )
+
+    testentry.save()
+
+    # reading entries
+    entries = testModelView.objects.all()
+    outputstring = "Printing all entries from testModelView: <br>"
+
+    for entry in entries:
+        outputstring += entry.name + "<br>"
+
+    # Read entry based on criteria
+    test = testModelView.objects.get(name="test")
+    outputstring += "Printing one entry <br>"
+    outputstring += test.name
+
+    # Update operation
+    testentry = testModelView(
+        website="www.google.com", mail="tester@google.com", name="tester", phonenumber="0987654321"
+    )
+
+    testentry.save()
+    outputstring += "Updating entry <br>"
+
+    testentry = testModelView.objects.get(name='tester')
+    testentry.name = 'testergoogle'
+    testentry.save
+
+    return HttpResponse(outputstring)
+
+
+def showallentries(request):
+    entries = testModelView.objects.all()
+    outputstring = "Printing all entries from db <br>"
+
+    for entry in entries:
+        outputstring += "name: " + entry.name + " mail: " + entry.mail + " website: " + entry.website + " phone number: " + str(entry.phonenumber) + "<br>"
+
+    return HttpResponse(outputstring)
